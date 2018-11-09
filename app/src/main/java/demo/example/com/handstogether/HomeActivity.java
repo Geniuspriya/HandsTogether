955a5e893e917d0b57b9fc3d152d30c38cb95391
Geniuspriya <inspiregeniussquad@gmail.com>
2018-11-09 13:25:17 +0530

    Test

---
+++ app/src/main/java/demo/example/com/handstogether/HomeActivity.java
@@ -0,0 +1,60 @@
+package demo.example.com.handstogether;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    private EditText editTextMobile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        editTextMobile = findViewById(R.id.editTextMobile);
+        final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.buttonContinue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mobile = editTextMobile.getText().toString().trim();
+
+                if(mobile.isEmpty() || mobile.length() < 10){
+                    editTextMobile.setError("Enter a valid mobile");
+                    editTextMobile.requestFocus();
+                    return;
+                }
+
+
+
+
+
+
+
+
+                Intent intent = new Intent(HomeActivity.this, VerifyPhoneActivity.class);
+                intent.putExtra("mobile", mobile);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(FirebaseAuth.getInstance().getCurrentUser()!= null)
+        {
+            Intent intent = new Intent( this, ProfileActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+}
